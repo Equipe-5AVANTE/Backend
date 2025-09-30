08194961781e501aa7c921bfdd1127a26ced4da2
Ingrid Oliveira <oligrid20@gmail.com>
2025-09-30 06:45:18 -0300

    refectore: rota de front end sem virgula ao final

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ import cors from "cors";
 import { Rotas } from "./src/routes/routes.js";
 
 const app = express();
-
 const corsOptions = {
   origin: [
     "http://localhost:5173",
-    "https://7l685mtv-5173.brs.devtunnels.ms, https://frontend-ckkn.onrender.com" ],
+    "https://7l685mtv-5173.brs.devtunnels.ms",
+    "https://frontend-ckkn.onrender.com",
+  ],
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
